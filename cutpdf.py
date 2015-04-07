707b9ebcdb3cc679dd1c283ff8257c467b37e5ab
psicobyte <psicobyte@gmail.com>
2015-04-07 10:47:30 +0200

    Funcionando

--- cutpdf.py
+++ cutpdf.py
@@ -15,3 +15,32 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import copy, os, pyPdf, sys
+
+pdf_input = file(sys.argv[1], "rb")
+
+nombre_output = sys.argv[2]
+
+pdf_output = file(nombre_output, "wb")
+
+output = pyPdf.PdfFileWriter()
+input1 = pyPdf.PdfFileReader(pdf_input)
+
+pg= input1.getNumPages()
+
+for i in range(0,pg):
+    pagina1 = input1.getPage(i)
+    pagina2 = copy.copy(pagina1)
+
+    corte= (pagina1.mediaBox.getUpperRight_x() / 2, pagina1.mediaBox.getUpperRight_y())
+
+    pagina1.mediaBox.upperRight = corte
+    output.addPage(pagina1)
+
+    pagina2.mediaBox.upperLeft = corte
+    output.addPage(pagina2)
+
+output.write(pdf_output)
+
+pdf_output.close
